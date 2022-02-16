a3ab7ded50c81ec64a4435ccf8738d9f72bfe456
Ruben van Baarle <rubnvb@xs4all.nl>
2022-02-16 20:40:26 +0100

    Initialization of clojure project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jepsen.rqlite
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/jepsen/rqlite_test.clj
@@ -0,0 +1,7 @@
+(ns jepsen.rqlite-test
+  (:require [clojure.test :refer :all]
+            [jepsen.rqlite :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
