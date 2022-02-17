bced7e4118d6be6ba3da65695ff4d623014bc85c
Ruben van Baarle <rubnvb@xs4all.nl>
2022-02-17 15:56:12 +0100

    Attempt at connecting to nodes through docker exec

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject jepsen.rqlite "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Testing rqlite with Jepsen"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :main jepsen.rqlite
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [jepsen "0.2.6"]]
   :repl-options {:init-ns jepsen.rqlite})
--- src/jepsen/rqlite.clj
+++ src/jepsen/rqlite.clj
@@ -1,6 +1,67 @@
-(ns jepsen.rqlite)
+(ns jepsen.rqlite
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [jepsen [cli :as cli]
+             [control :as c]
+             [db :as db]
+             [tests :as tests]]
+            [jepsen.control.util :as cu]
+            [jepsen.control.docker :as docker]
+            [jepsen.os.debian :as debian]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def dir "/opt/rqlite")
+(def binary "rqlited")
+(def logfile (str dir "/rqlite.log"))
+(def pidfile (str dir "/rqlite.pid"))
+
+(defn client-addr [node]
+  (str node ":4001"))
+(defn peer-addr [node]
+  (str node ":4002"))
+
+(defn db
+  "rqlite DB for a particular version"
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing rqlite" version)
+      (c/su
+        (let [url (str "https://github.com/rqlite/rqlite/releases/download/" version
+                       "/rqlite-" version "-linux-amd64.tar.gz")]
+          (cu/install-archive! url dir))
+        (cu/start-daemon!
+          {:logfile logfile
+           :pidfile pidfile
+           :chdir   dir}
+          binary
+          :-node-id     (name node)
+          :-http-addr   (client-addr node)
+          :-raft-addr   (peer-addr node)))
+      (Thread/sleep 10000))
+
+    (teardown! [_ test node]
+      (info node "tearing down rqlite")
+      (cu/stop-daemon! binary pidfile)
+      (c/su (c/exec :echo :rm :-rf dir)))))
+
+
+(defn rqlite-test
+  "Given an options map from the command line runner (e.g. :nodes, :ssh,
+  :concurrency, ...), constructs a test map."
+  [opts]
+  (merge tests/noop-test
+         opts
+         {:name            "rqlite"
+          :os              debian/os
+          :db              (db "v7.3.1")
+          :remote          docker/docker
+          :pure-generators true
+          :sudo            false}))
+
+(defn -main
+  "Handles command line arguments. Can either run a test, or a web server for
+  browsing results."
+  [& args]
+  (cli/run! (merge (cli/single-test-cmd {:test-fn rqlite-test})
+                   (cli/serve-cmd))
+            args))
