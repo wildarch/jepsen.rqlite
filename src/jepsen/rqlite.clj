5fce2a5019770238f814224095900e40fdc459dd
Ruben van Baarle <rubnvb@xs4all.nl>
2022-02-17 21:17:58 +0100

    Working connection to nodes

--- src/jepsen/rqlite.clj
+++ src/jepsen/rqlite.clj
@@ -6,8 +6,9 @@
              [db :as db]
              [tests :as tests]]
             [jepsen.control.util :as cu]
-            [jepsen.control.docker :as docker]
-            [jepsen.os.debian :as debian]))
+            [jepsen.os.ubuntu :as ubuntu]
+            [jepsen.rqlite.client as client])
+  (:import (jepsen.rqlite.client Client)))
 
 (def dir "/opt/rqlite")
 (def binary "rqlited")
@@ -18,6 +19,10 @@
   (str node ":4001"))
 (defn peer-addr [node]
   (str node ":4002"))
+
+(defn r [_ _] {:type :invoke, :f :read, :value nil})
+(defn w [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
+(defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 5) (rand-int 5)]})
 
 (defn db
   "rqlite DB for a particular version"
@@ -30,19 +35,22 @@
                        "/rqlite-" version "-linux-amd64.tar.gz")]
           (cu/install-archive! url dir))
         (cu/start-daemon!
-          {:logfile logfile
-           :pidfile pidfile
-           :chdir   dir}
+          {:logfile       logfile
+           :pidfile       pidfile
+           :make-pidfile? true
+           :chdir         dir}
           binary
-          :-node-id     (name node)
-          :-http-addr   (client-addr node)
-          :-raft-addr   (peer-addr node)))
-      (Thread/sleep 10000))
+          :-node-id (name node)
+          :-http-addr (client-addr node)
+          :-raft-addr (peer-addr node)
+          "~/node_data"))
+      (Thread/sleep 30000))
 
     (teardown! [_ test node]
       (info node "tearing down rqlite")
-      (cu/stop-daemon! binary pidfile)
-      (c/su (c/exec :echo :rm :-rf dir)))))
+      (c/su (cu/stop-daemon! binary pidfile)
+            (c/exec :rm :-rf dir)
+            (c/exec :rm :-rf "~/node_data")))))
 
 
 (defn rqlite-test
@@ -52,11 +60,16 @@
   (merge tests/noop-test
          opts
          {:name            "rqlite"
-          :os              debian/os
+          :os              ubuntu/os
           :db              (db "v7.3.1")
-          :remote          docker/docker
-          :pure-generators true
-          :sudo            false}))
+          :client          (Client. nil)
+          :ssh             {:username                 "rqlite"
+                            :password                 "rqlite"
+                            :strict-host-key-checking false
+                            :port                     22
+                            :sudo-password            "rqlite"
+                            }
+          :pure-generators true}))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
